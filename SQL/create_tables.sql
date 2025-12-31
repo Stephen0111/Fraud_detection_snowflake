de8c5ee03dc508b606e1397e30c286cc417181ad
Stephen0111 <86751563+Stephen0111@users.noreply.github.com>
2025-12-31 04:29:51 +0000

    Create RAW.TRANSACTIONS table with relevant fields

---
+++ SQL/create_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE RAW.TRANSACTIONS (
+  TransactionID STRING,
+  AccountID STRING,
+  TransactionAmount FLOAT,
+  TransactionDate TIMESTAMP,
+  TransactionType STRING,
+  Location STRING,
+  DeviceID STRING,
+  IPAddress STRING,
+  MerchantID STRING,
+  Channel STRING,
+  CustomerAge INT,
+  CustomerOccupation STRING,
+  TransactionDuration FLOAT,
+  LoginAttempts INT,
+  AccountBalance FLOAT,
+  PreviousTransactionDate TIMESTAMP
+);
